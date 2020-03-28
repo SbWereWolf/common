fc67d18a2217747b702753326d7ae5679e526179
Вольхин Николай <ulfnew@gmail.com>
2020-03-28 10:39:48 +0500

    Create run-any-script-on-document-ready.js

---
+++ run-any-script-on-document-ready.js
@@ -0,0 +1,13 @@
+// declaration
+function docReady(fn) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(fn, 100);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+// applying
+docReady(function () {
+    // some code here
+});
